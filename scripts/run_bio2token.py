508908806d6f0d27ec6757de5e17b0546ab7158c
Olivia Viessmann <oviessmann@flagshippioneering.com>
2024-09-27 22:19:42 +0000

    new utils

--- scripts/run_bio2token.py
+++ scripts/run_bio2token.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 from typing import *
 import torch
-from torch.utils.data import DataLoader
-from loguru import logger
 import argparse
+import os
 
 from bio2token.models.fsq_ae import FSQ_AE
-from functools import partial
 
 
 from bio2token.utils.utils import *
@@ -15,7 +13,9 @@ from bio2token.utils.pdb import *
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--tokenizer", type=str, default="protein2token")
+    parser.add_argument("--tokenizer", type=str, default="bio2token")
+    parser.add_argument("--pdb_file", type=str, default="6vn1.pdb")
+    parser.add_argument("--res_type", type=str, default="AA")
     args = parser.parse_args()
 
     if args.tokenizer == "mol2token":
@@ -23,7 +23,7 @@ def main():
         config_model = load_config("/home/pi-user/bio2token/configs/tokenizer.yaml")
     if args.tokenizer == "protein2token":
         checkpoint = "/home/pi-user/bio2token/checkpoints/protein2token.ckpt"
-        config_model = load_config("/home/pi-user/bio2token/configs/toeknizer.yaml")
+        config_model = load_config("/home/pi-user/bio2token/configs/tokenizer.yaml")
     if args.tokenizer == "bio2token":
         checkpoint = "/home/pi-user/bio2token/checkpoints/bio2token.ckpt"
         config_model = load_config("/home/pi-user/bio2token/configs/tokenizer.yaml")
@@ -44,17 +44,46 @@ def main():
 
     # Print the number of parameters
     print(f"Number of parameters: {count_parameters(model)}")
-    protein = pdb_2_dict("/home/pi-user/bio2token/examples/6vn1.pdb")
-    batch = pdb_to_batch(config_model, protein)
-    batch["seq_type"] = "protein"
+    biomolecule = pdb_2_dict(
+        os.path.join("/home/pi-user/bio2token/examples/ground_truth", args.pdb_file), res_type=args.res_type, chains=["A"]
+    )
+    batch = pdb_to_batch(config_model, biomolecule)
+    batch["seq_type"] = args.res_type
     with torch.no_grad():
         out = model.step(batch, mode="inference")
 
     print("RMSD error: ", out["rmsd"])
     print("TM-score: ", out["tm"])
+    # Cut out zeros in batch
+    gt = out["coords_gt"][:, : biomolecule["atom_length"], :].squeeze(0).cpu().numpy()
+    gt = np.split(gt, gt.shape[0])
+    recon = out["coords_pred_kabsch_all"][:, : biomolecule["atom_length"], :].squeeze(0).cpu().numpy()
+    recon = np.split(recon, recon.shape[0])
 
-    # pdb_dict = pdb_dict(coords = [], batch)
-    # pdb_dict_to_file(pdb_dict, pdb_file_path="/home/pi-user/bio2token/examples/6vn1_gt_recon.pdb")
+    pdb_dict_gt = to_pdb_dict(
+        coords=gt,
+        atom_names=biomolecule["atom_names"],
+        continuous_res_ids=biomolecule["continuous_res_ids"],
+        residue_names=biomolecule["res_names"],
+    )
+    pdb_dict_recon = to_pdb_dict(
+        coords=recon,
+        atom_names=biomolecule["atom_names"],
+        continuous_res_ids=biomolecule["continuous_res_ids"],
+        residue_names=biomolecule["res_names"],
+    )
+    # print(pdb_dict_recon)
+    pdb_dict_to_file(
+        pdb_dict_gt,
+        pdb_file_path=os.path.join("/home/pi-user/bio2token/examples/recon", f"{args.pdb_file}_gt.pdb"),
+        res_type=args.res_type,
+    )
+
+    pdb_dict_to_file(
+        pdb_dict_recon,
+        pdb_file_path=os.path.join("/home/pi-user/bio2token/examples/recon", f"{args.pdb_file}_recon.pdb"),
+        res_type=args.res_type,
+    )
 
 
 if __name__ == "__main__":
