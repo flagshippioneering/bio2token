b7505badec7deec54c6105096c4f606cf315d372
Olivia Viessmann <oviessmann@flagshippioneering.com>
2024-09-30 15:24:19 +0000

    better script

--- scripts/run_bio2token.py
+++ scripts/run_bio2token.py
@@ -14,19 +14,17 @@ from bio2token.utils.pdb import *
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--tokenizer", type=str, default="bio2token")
-    parser.add_argument("--pdb_file", type=str, default="6n64.pdb")
-    parser.add_argument("--res_type", type=str, default="AA")
+    parser.add_argument("--pdb", type=str, default="6n64.pdb")
+    parser.add_argument("--chains", type=str, default=None)
     args = parser.parse_args()
 
     if args.tokenizer == "mol2token":
         checkpoint = "/home/pi-user/bio2token/checkpoints/mol2token.ckpt"
-        config_model = load_config("/home/pi-user/bio2token/configs/tokenizer.yaml")
     if args.tokenizer == "protein2token":
         checkpoint = "/home/pi-user/bio2token/checkpoints/protein2token.ckpt"
-        config_model = load_config("/home/pi-user/bio2token/configs/tokenizer.yaml")
     if args.tokenizer == "bio2token":
         checkpoint = "/home/pi-user/bio2token/checkpoints/bio2token.ckpt"
-        config_model = load_config("/home/pi-user/bio2token/configs/tokenizer.yaml")
+    config_model = load_config("/home/pi-user/bio2token/configs/tokenizer.yaml")
 
     # Instantiate and move the model to GPU
     model = FSQ_AE(config_model).cuda()
@@ -45,10 +43,16 @@ def main():
     # Print the number of parameters
     print(f"Number of parameters: {count_parameters(model)}")
     biomolecule = pdb_2_dict(
-        os.path.join("/home/pi-user/bio2token/examples/ground_truth", args.pdb_file), res_type=args.res_type, chains=["A"]
+        os.path.join("/home/pi-user/bio2token/examples/ground_truth", args.pdb + ".pdb"), chains=args.chains
     )
     batch = pdb_to_batch(config_model, biomolecule)
-    batch["seq_type"] = args.res_type
+    batch["seq_type"] = "AA"
+
+    print(f"PDB file: {args.pdb}")
+    print(f"Chains: {args.chains}")
+    print(f"Number of residues: {len(biomolecule['seq'])}")
+    print(f"Number of atoms: {len(biomolecule['atom_names'])}")
+
     with torch.no_grad():
         out = model.step(batch, mode="inference")
 
@@ -65,24 +69,24 @@ def main():
         atom_names=biomolecule["atom_names"],
         continuous_res_ids=biomolecule["continuous_res_ids"],
         residue_names=biomolecule["res_names"],
+        res_types=biomolecule["res_types"],
     )
     pdb_dict_recon = to_pdb_dict(
         coords=recon,
         atom_names=biomolecule["atom_names"],
         continuous_res_ids=biomolecule["continuous_res_ids"],
         residue_names=biomolecule["res_names"],
+        res_types=biomolecule["res_types"],
     )
     # print(pdb_dict_recon)
     pdb_dict_to_file(
         pdb_dict_gt,
-        pdb_file_path=os.path.join("/home/pi-user/bio2token/examples/recon", f"{args.pdb_file}_gt.pdb"),
-        res_type=args.res_type,
+        pdb_file_path=os.path.join("/home/pi-user/bio2token/examples/recon", f"{args.pdb}_{args.tokenizer}_gt.pdb"),
     )
 
     pdb_dict_to_file(
         pdb_dict_recon,
-        pdb_file_path=os.path.join("/home/pi-user/bio2token/examples/recon", f"{args.pdb_file}_recon.pdb"),
-        res_type=args.res_type,
+        pdb_file_path=os.path.join("/home/pi-user/bio2token/examples/recon", f"{args.pdb}_{args.tokenizer}_recon.pdb"),
     )
 
 
